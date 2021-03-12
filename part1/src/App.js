f247809b34f49341306a4b84a7b41380e11322de
justin-tse <xiefahit@gmail.com>
2021-03-12 23:21:04 +0800

    Pass data to components using props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}
+      </p>
     </div>
   )
 }
@@ -12,9 +14,8 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />      
+      <Hello name="George"/>
+      <Hello name="Daisy"/>
     </div>
   )
 }
