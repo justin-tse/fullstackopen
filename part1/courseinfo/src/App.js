e9e9f6bfd96629836d2cf84e2e82f81ef2559521
justin-tse <xiefahit@gmail.com>
2021-03-15 23:34:46 +0800

    Refactor step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,20 +2,11 @@ import React from 'react';
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content />
     </div>
   );
 };
@@ -26,7 +17,25 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Content = () => {
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
   return (
     <p>{props.part} {props.exercises}</p>
   );
