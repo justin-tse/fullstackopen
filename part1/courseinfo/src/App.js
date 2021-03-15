ba3ed0f5f6e40663dfa9ac972f0f522b415cb671
justin-tse <xiefahit@gmail.com>
2021-03-15 21:19:54 +0800

    Refactor Header

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,18 +11,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
     </div>
+  );
+};
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
   );
 };
 
