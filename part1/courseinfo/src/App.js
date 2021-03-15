99d6378e45359f871ae879c2a24d08501ec109f3
justin-tse <xiefahit@gmail.com>
2021-03-15 22:53:54 +0800

    Refactor step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,6 +15,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   );
 };
@@ -28,6 +29,12 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <p>{props.part} {props.exercises}</p>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   );
 };
 
